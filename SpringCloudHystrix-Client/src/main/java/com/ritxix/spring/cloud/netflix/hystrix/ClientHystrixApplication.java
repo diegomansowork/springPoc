889e8f49e970fbbc718b97f34762acff06cad898
ritxix <ritxix@gmail.com>
2018-12-07 20:21:16 +0100

    Hystrix DashBoard POC

--- SpringCloudHystrix-Client/src/main/java/com/ritxix/spring/cloud/netflix/hystrix/ClientHystrixApplication.java
+++ SpringCloudHystrix-Client/src/main/java/com/ritxix/spring/cloud/netflix/hystrix/ClientHystrixApplication.java
@@ -8,17 +8,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @RestController
 @SpringBootApplication
 public class ClientHystrixApplication {
 
+	
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+	
 	@Autowired
 	private BreakerPatternService breakerPatternService;
 
